824f75bb5d4bb4cb0f4aaaaabd3ec9af6d302e27
Roshan Ashok <roashok@icloud.com>
2024-11-28 01:30:33 +0400

    simplified margin while creating sidebar and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ToolbarComponent],
+  imports: [RouterOutlet,MatSidenavModule,CommonModule,MatToolbarModule,MatIconModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+  collapsed = signal(false)
+  sideWidth = computed(()=>this.collapsed() ? '65px' : '250px') 
 }
